ac685e02cfa048b3d9ea78f295aff4a7b5004ff0
bitTal <albafjez@gmail.com>
2016-06-23 12:51:11 +0200

    Slider module structured

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,12 +24,3 @@
 
 
 
-$('.slider-container').ready( function() {
-  $('.slider-container').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true
-  });
-});
-
---
+++ app/assets/javascripts/static.js
@@ -0,0 +1,14 @@
+
+
+function setSlider() {
+  $('.slider-container').ready( function() {
+    $('.slider-container').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true
+    });
+  });
+}
+
+setSlider();
