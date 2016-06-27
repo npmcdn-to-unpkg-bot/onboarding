d27e4100d0ad170a6447d94cf7f05a590437a8d0
bitTal <albafjez@gmail.com>
2016-06-27 16:04:40 +0200

    Dots menus established

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -16,3 +16,62 @@ $(document).on('page:change', function () {
   setSlider();
 });
 
+function getSectionsPositions() {
+  let positions = {};
+  positions[$('#intro').offset().top] = 'intro';
+  positions[$('#slider').offset().top] = 'slider';
+  positions[$('#participate').offset().top] = 'participate';
+  positions[$('#events').offset().top] = 'events';
+  positions[$('#community').offset().top] = 'community';
+  positions[$('#activity').offset().top] = 'activity';
+  positions[$('#about').offset().top] = 'about';
+
+  return positions;
+}
+
+$(window).scroll(function() {
+  let positions = getSectionsPositions();
+  const windowTop = $(window).scrollTop();
+  const key = setItemKey(positions, windowTop);
+
+  clearActiveDots();
+  setActiveDot(positions, key);
+});
+
+
+function setItemKey(positions, windowTop) {
+  let keys = Object.keys(positions);
+  let key = 0;
+
+  for(let i = 0; i < keys.length; i++) {
+    if(windowTop < keys[i] && i !== 0) {
+      key = i-1;
+      break;
+    }
+    else if(windowTop + $(window).height() == $(document).height()) {
+      key = keys.length-1;
+      break;
+    }
+    else if(windowTop > keys[i] || i === keys.length-1) {
+      key = i;
+    }
+  }
+  return key;
+}
+
+
+function clearActiveDots() {
+  let items = document.getElementsByClassName('dot-item');
+
+  for(let i = 0; i < items.length; i++) {
+    const item = document.getElementsByClassName('dot-item')[i];
+    let itemClass = item.getAttribute('class');
+    item.setAttribute('class', itemClass.replace('-active', ''));
+  }
+}
+
+function setActiveDot(positions, key) {
+  let keys = Object.keys(positions);
+  let elementClass = document.getElementById(positions[keys[key]] + '-dot').getAttribute('class');
+  document.getElementById(positions[keys[key]] + '-dot').setAttribute('class', elementClass + ' -active');
+}
