c7e8a820794cfa64472bcb6f7ed9d109c2d8def4
Tiago Garcia <tiagojsag@gmail.com>
2016-07-12 15:09:39 +0200

    Enumeration values

--- app/enumerations/task_types.rb
+++ app/enumerations/task_types.rb
@@ -1,7 +1,6 @@
 class TaskTypes < EnumerateIt::Base
   associate_values(
-      type_3: [1, 'Type 1'],
-      type_2: [2, 'Type 2'],
-      type_1: [3, 'Type 3']
+      osm_tasking_manager: [1, 'OSM Tasking Manager'],
+      to_fix: [2, 'To Fix']
   )
 end
