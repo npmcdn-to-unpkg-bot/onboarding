cf9104b0f49fe5cfe507b08b34e71282cf61a6d6
Clara Linos <clara.linos@gmail.com>
2016-08-05 14:23:17 +0200

    tabs working

--- app/assets/javascripts/components/pages/EventsPageView.js
+++ app/assets/javascripts/components/pages/EventsPageView.js
@@ -5,28 +5,42 @@ import DataTableView from './../DataTableView'
 
 class CampaingView extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "tab1"
+    };
+  }
+
   componentDidMount() {
     /* data will specify what kind of section will be rendered */
     this.props.setEventsList();
     this.props.setTasksList();
   }
 
-  render() {
+  changeTab(tab) {
+    this.setState({activeTab: tab});
+  }
 
+  render() {
     return (
       <div>
         <div className="c-tabs">
           <ul className="tabs">
-            <li className="tab">Mapathon</li>
-            <li className="tab">Task</li>
+            <div className="wrap row">
+              <div className="col-md-12">
+                <li className="tab" onClick={this.changeTab.bind(this, "tab1")}>Mapathon</li>
+                <li className="tab" onClick={this.changeTab.bind(this, "tab2")}>Task</li>
+              </div>
+            </div>
           </ul>
           <div className="tabs-content">
-            <div className="tabs-panel">
+            <div className={`tabs-panel ${this.state.activeTab === "tab1" ? "-is-active" : ""}`}>
               <DataTableView
                 data={this.props.eventsList}
               />
             </div>
-            <div className="tabs-panel">
+            <div className="tabs-panel" className={`tabs-panel ${this.state.activeTab === "tab2" ? "-is-active" : ""}`}>
               <DataTableView
                 data={this.props.tasksList}
               />
