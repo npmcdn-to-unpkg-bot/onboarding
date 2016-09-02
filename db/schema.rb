ecbeb7fcd7b4c09c641528d27f91e4808fa67029
edbrett <e.j.a.brett@gmail.com>
2016-09-02 12:15:08 +0200

    add migrations for location name to events, change event req location to json

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901113046) do
+ActiveRecord::Schema.define(version: 20160902101124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,12 +54,13 @@ ActiveRecord::Schema.define(version: 20160901113046) do
   create_table "event_requests", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.string   "location"
+    t.json     "location"
     t.date     "potential_date"
     t.string   "experience_level"
     t.string   "email"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "location_name"
   end
 
   create_table "events", force: :cascade do |t|
@@ -79,6 +80,7 @@ ActiveRecord::Schema.define(version: 20160901113046) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "campaign_id"
+    t.string   "location_name"
     t.index ["campaign_id"], name: "index_events_on_campaign_id", using: :btree
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
