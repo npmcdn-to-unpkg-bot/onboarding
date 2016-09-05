54dafce5c8396b51c36a5def0f0117b4bc4898e5
edbrett <e.j.a.brett@gmail.com>
2016-09-05 18:44:35 +0200

    campaigns table in front end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  private
+
+  def sort_column
+    controller_name.classify.constantize.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,5 +1,12 @@
 class CampaignsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
+    @campaigns = Campaign.search(params[:search]).order(sort_column + " " + sort_direction).paginate(page: params[:page], per_page: 9)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
