6f871417ed83251caa89e24eff80f11e073683d0
edbrett <e.j.a.brett@gmail.com>
2016-09-05 20:27:30 +0200

    add conditional link for campaigns url

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,11 @@ class TasksController < ApplicationController
   before_action only: [:show]
 
   def index
+    @tasks = Task.search(params[:search]).order(sort_column + " " + sort_direction).paginate(page: params[:page], per_page: 9).where(status: 3)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
