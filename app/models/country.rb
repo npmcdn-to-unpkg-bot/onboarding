5bf5f206583217dccde5f124dc874caae315f369
edbrett <e.j.a.brett@gmail.com>
2016-09-05 15:41:53 +0200

    logic for disabling marker on tasks location

--- app/models/country.rb
+++ app/models/country.rb
@@ -12,4 +12,10 @@
 #
 
 class Country < ApplicationRecord
+  validates :name, :iso, :shp_url, :geojson_url, :presence => true
+  validates :iso, :uniqueness => { case_sensitive: false }
+
+  def iso=(val)
+    write_attribute :iso, val.upcase
+  end
 end
