524ae4b3118877673be8bc8d173f8e4d418be1d9
edbrett <e.j.a.brett@gmail.com>
2016-09-07 20:26:42 +0200

    complete tasks tables search separation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,13 @@ class TasksController < ApplicationController
   before_action only: [:show]
 
   def index
-    @tasks = Task.search(params[:search]).order(sort_column + " " + sort_direction).paginate(page: params[:page], per_page: 9).where(status: 3)
+    if params[:campaign_id]
+      @tasks = Campaign.find(params[:campaign_id]).tasks
+    else
+      @tasks = Task.all
+    end
+    @tasks = @tasks.search(params[:search_tasks]).order(sort_column + " " + sort_direction).
+      paginate(page: params[:page], per_page: 9).where(status: TaskStatus::LIVE)
     respond_to do |format|
       format.html
       format.js
