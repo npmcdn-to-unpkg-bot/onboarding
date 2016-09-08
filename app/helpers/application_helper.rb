daa58609d9639e1c7b085ab4ce51859d505a1e7b
Simao Belchior <simaobelchior@gmail.com>
2016-09-08 10:41:50 +0100

    Set the nested path if the events or tasks are being rendered from the campaigns controller. removes duplicated code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,11 @@ module ApplicationHelper
     controller.include?(params[:controller]) ? 'active ': ''
   end
 
-  def sortable(column, title = nil)
+  def sortable(column, title = nil, custom_path = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to "<span>#{title}</span>".html_safe, params.merge(sort: column, direction: direction).permit!, {class: css_class}
+    path = (custom_path ? custom_path : params).merge(sort: column, direction: direction).permit!
+    link_to "<span>#{title}</span>".html_safe, path, {class: css_class}
   end
 end
